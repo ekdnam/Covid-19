6594692bce03ca24aaa5e2ad715c944a3043bddf
ekdnam <adeetya.m@gmail.com>
2020-10-26 23:43:41 +0530

    modify batch file

--- get_data/tweetsBatch.bat
+++ get_data/tweetsBatch.bat
@@ -1,5 +1,12 @@
 @echo off
 @REM activate
+call :twitterFunc
+:twitterFunc
 py getTweets2.py
-git add . && git commit - m "add tweets" && git push origin master
+git add .
+git commit -m "add tweets"
+git push origin master
+timeout 900
+call :twitterFunc
+EXIT /B 0
 pause
